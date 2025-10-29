cfd8627d8c60ba23e0a60fee3ebf790823031370
Sameerali0 <sameer.ali.professional@gmail.com>
2025-10-29 23:29:56 +0500

    added start button and styling

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const gameArea = document.getElementById("game-area")
+const startBtn = document.getElementById("start-btn")
+
+startBtn.addEventListener("click", () => {
+    alert("abhi game bana hi nhi")
+})
