ea54a105c00129eb11eca7e3ea0d1f9f1b9798ea
Sameerali0 <sameer.ali.professional@gmail.com>
2025-10-30 10:43:11 +0500

    added random ghost position

--- script.js
+++ script.js
@@ -5,5 +5,16 @@ startBtn.addEventListener("click", () => {
     const ghost = document.createElement("div")
     ghost.classList.add("ghost")
 
+    const ghostSize = 70;
+
+    const limitX = gameArea.clientWidth - ghostSize;
+    const limitY = gameArea.clientHeight - ghostSize;
+
+    const randomPosX = Math.random() * limitX
+    const randomPosY = Math.random() * limitY;
+
+    ghost.style.left = `${randomPosX}px`;
+    ghost.style.top = `${randomPosY}px`
+
     gameArea.appendChild(ghost)
 })
